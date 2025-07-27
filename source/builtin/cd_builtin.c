5c7f37d9339b7df39e705ff1d8a8bceb9ad4f73f
Alisa Harutyunyan <alisharu@c2r4s3.42yerevan.am>
2025-07-27 21:07:14 +0400

    adding pwd case

--- source/builtin/cd_builtin.c
+++ source/builtin/cd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: alisharu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 20:27:17 by alisharu          #+#    #+#             */
-/*   Updated: 2025/07/13 12:53:29 by alisharu         ###   ########.fr       */
+/*   Updated: 2025/07/22 10:24:15 by alisharu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,17 +49,12 @@ int	check_too_many_dirs(char **args, t_env *env)
 
 void	cd_builtin(char **args, t_env *env)
 {
-	char	old_pwd[PATH_MAX];
+	char	*old_pwd;
 	char	new_pwd[PATH_MAX];
 	char	*path;
 
 	if (!check_too_many_dirs(args, env))
 		return ;
-	if (!getcwd(old_pwd, sizeof(old_pwd)))
-	{
-		perror("minishell: cd: getcwd");
-		return ;
-	}
 	if (!args[1])
 	{
 		path = handle_cd_without_dir(args, env);
@@ -68,14 +63,19 @@ void	cd_builtin(char **args, t_env *env)
 	}
 	else
 		path = args[1];
+	old_pwd = env_get(env, "PWD")->value;
 	if (chdir(path) != 0)
 	{
+		env->shell->exit_code = 1;
 		perror("minishell: cd");
 		return ;
 	}
 	if (!getcwd(new_pwd, sizeof(new_pwd)))
 	{
-		perror("minishell: cd: error retrieving current directory");
+		printf("cd: error retrieving current directory: getcwd: ");
+		printf("cannot access parent directories: No such file or directory\n");
+		env->shell->pwd = ft_strjoin(env_get(env, "PWD")->value, "/..");
+		env_set(env, "PWD", env->shell->pwd, 1);
 		return ;
 	}
 	env_set(env, "OLDPWD", old_pwd, 1);
